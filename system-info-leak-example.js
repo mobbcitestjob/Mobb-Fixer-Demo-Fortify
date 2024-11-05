fcc237175d69dc6ceb44f93c48c38e002c8f42be
Mobb autofixer <git@mobb.ai>
2024-11-05 09:34:46 +0000

    System Information Leak fix by mobb-47b82327-37ed-4f61-970c-2fe1cf2c9c46

--- system-info-leak-example.js
+++ system-info-leak-example.js
@@ -4,7 +4,7 @@ const fs = require('node:fs/promises');
 http.createServer((req, res) => {
     fs.readFile("./no-file.txt").catch((e) => {
         // here's a system information leak:
-        console.error(e);
+        /* Logging errors in the console.error() may lead to information leakage. Instead, you should write the error to your logger like so: LOGGER.error("Unexpected error: mobb-b0982cf42637537d5c3e7817513468c8"); */;
     });
     res.writeHead(200);
     res.end("👋");
